6729870e96763551658cbf8e17c897530d5fed8d
25sDungs <135621389+25sDungs@users.noreply.github.com>
2023-08-15 14:42:22 +0700

    Add files via upload

--- js/gioi-thieu.js
+++ js/gioi-thieu.js
@@ -1,19 +1,27 @@
 // Lắng nghe sự kiện cuộn trang
 window.addEventListener('scroll', function () {
-    var backToTopBtn = document.getElementById('backToTopBtn');
-  
-    // Hiển thị nút "Back to top" khi cuộn xuống 10px
-    if (window.scrollY > 10) {
-      backToTopBtn.classList.add('show');
-    } else {
-      backToTopBtn.classList.remove('show');
-    }
+  var backToTopBtn = document.getElementById('backToTopBtn');
+
+  // Hiển thị nút "Back to top" khi cuộn xuống 10px
+  if (window.scrollY > 10) {
+    backToTopBtn.classList.add('show');
+  } else {
+    backToTopBtn.classList.remove('show');
+  }
+});
+
+// Xử lý sự kiện khi nhấp vào nút "Back to top"
+document.getElementById('backToTopBtn').addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Cuộn mượt đến đầu trang
   });
-  
-  // Xử lý sự kiện khi nhấp vào nút "Back to top"
-  document.getElementById('backToTopBtn').addEventListener('click', function () {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth' // Cuộn mượt đến đầu trang
-    });
-  });
+});
+$(window).scroll(function () {
+  let scrollTop = $(window).scrollTop();
+  if (scrollTop > 160) {
+    $('.nav').addClass("fixed");
+  } else {
+    $(".nav").removeClass("fixed");
+  }
+}) 
--- js/ho_so.js
+++ js/ho_so.js
@@ -84,4 +84,31 @@ $(document).ready(() => {
         else
             $("#DanhSachSinhVien>div").removeClass("display-none");
     });
+    window.addEventListener('scroll', function () {
+        var backToTopBtn = document.getElementById('backToTopBtn');
+    
+        // Hiển thị nút "Back to top" khi cuộn xuống 10px
+        if (window.scrollY > 10) {
+            backToTopBtn.classList.add('show');
+        } else {
+            backToTopBtn.classList.remove('show');
+        }
+    });
+    
+    // Xử lý sự kiện khi nhấp vào nút "Back to top"
+    document.getElementById('backToTopBtn').addEventListener('click', function () {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Cuộn mượt đến đầu trang
+        });
+    });
 })
+
+$(window).scroll(function () {
+    let scrollTop = $(window).scrollTop();
+    if (scrollTop > 160) {
+        $('.nav').addClass("fixed");
+    } else {
+        $(".nav").removeClass("fixed");
+    }
+}) 
