e0965504a307a03b2ea299a168914025c9739bac
25sDungs <135621389+25sDungs@users.noreply.github.com>
2023-08-15 12:02:49 +0700

    Delete makeforfun.js

--- js/makeforfun.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-    $("#taoSoMayMan").click(function () {
-        let h= document.getElementById("soMayMan");
-        h.innerText ="Số may mắn của bạn là: " + Math.floor(Math.random() * 100);
-    })
-})
-
-
